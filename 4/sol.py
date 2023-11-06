370fb1b3a7310d32db438f81b975543cecb7a8b9
Iker Lissarrague <iker.lissarrague@icloud.com>
2023-11-06 01:42:16 -0600

    some easy ones

---
+++ 4/sol.py
@@ -0,0 +1,23 @@
+from sys import stdin
+from collections import Counter
+
+lines = [line[:-1] for line in stdin]
+
+def part1(lines):
+    s = 0
+    for line in lines:
+        if len(set(line.split(" "))) == len(line.split(" ")):
+            s += 1
+    return s
+
+def part2(lines):
+    s = 0
+    for line in lines:
+        split = line.split(" ")
+        c = Counter("".join(sorted(w)) for w in split)
+        if len(c) == len(split):
+            s += 1
+    return s
+
+print(part1(lines))
+print(part2(lines))
